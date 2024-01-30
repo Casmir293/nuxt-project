72936bd50b1244f31186794124faa14a62e0da2a
Casmir Onyeka <casmir293@gmail.com>
2024-01-30 19:57:10 +0100

    added error functions

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <p>{{ product.title }}</p>
-    <p>{{ product.price }}</p>
-    <p>{{ product.id }}</p>
+    <ProductDetails :product="product" />
   </div>
 </template>
 
@@ -17,6 +15,10 @@ definePageMeta({
 
 // Fetch Products
 const { data: product } = await useFetch(productID, { key: id });
+
+if (!product.value) {
+  throw createError({ statusCode: 404, statusMessage: "Product not found" });
+}
 </script>
 
 <style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="grid grid-cols-4 gap-5">
-    <div v-for="p in products">
-      <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <product-card :product="p" />
+      </div>
     </div>
   </div>
 </template>
